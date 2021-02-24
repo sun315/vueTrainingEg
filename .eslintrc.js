b5c60c3148dcc04fdade8246ff018415f0d7c1a0
LAPTOP-P1VANO38\sunlau <yyllau@163.com>
2021-02-24 16:52:24 +0800

    qustions-dist

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,11 @@ module.exports = {
     parser: "babel-eslint"
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': 'off',
+    "no-debugger":'off',
+    'no-unused-vars':'off',//意思就是不让eslint校验我定义有没有用了。
+    // "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "prettier/prettier": "off"
   }
 };
